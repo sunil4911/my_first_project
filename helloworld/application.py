a735bd2fa41a182c9e9ec93d2d582413144be901
sunil <sunil4911@gmail.com>
2020-12-28 14:40:43 -0500

    created my sql connector

--- helloworld/application.py
+++ helloworld/application.py
@@ -1,13 +1,24 @@
 #!flask/bin/python
 import json
+import mysql.connector
 from flask import Flask, Response
 from helloworld.flaskrun import flaskrun
 
 application = Flask(__name__)
 
+connection = mysql.connector.connect(
+    host="my-first-project-db.crnpdg0lmdqp.us-east-2.rds.amazonaws.com",
+    user="admin",
+    password="M012599g",
+    database="my_first_project_db"
+)
+
+db = connection.cursor(dictionary=True)
 @application.route('/', methods=['GET'])
 def get():
-    return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+    db.execute("select * from people")
+    result = db.fetchall()
+    return Response(json.dumps(result), mimetype='application/json', status=200)
 
 @application.route('/', methods=['POST'])
 def post():
